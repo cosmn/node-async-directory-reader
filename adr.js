d91aac2981fcda5e48ebdc9307b104cad66f0403
Cosmin Budinschi <budinschi.cosmin@gmail.com>
2017-06-06 03:28:12 +0300

    js compatibility down to node 0.8

--- adr.js
+++ adr.js
@@ -1,44 +1,47 @@
 'use strict';
 
-const path = require('path');
-const fs = require('fs');
+var path = require('path');
+var fs = require('fs');
 
-module.exports = function adr(dir, callback){
+var adr = module.exports = function adr(dir, callback){
 
-	let files = [], errors = []
+    var files = [], errors = []
 
-	fs.readdir( dir, function(err, paths){
-		if(err) return callback([]);
+    fs.readdir( dir, function(err, paths){
+        if(err) return callback([err]);
 
-		let paths_count = paths.length
-		let paths_processed = 0
+        var paths_count = paths.length
+        var paths_processed = 0
 
-		paths.forEach(function(file){
-			let processing_path = path.resolve(dir + path.sep + file)
-			fs.lstat( processing_path, function(err, stat){
-				if( err ){
-					errors.push(err)
-					paths_processed++
-					done(paths_count, paths_processed, errors, files, callback)
-				}else if( stat && stat.isDirectory() ){
-					rdr( processing_path, function(err, new_files){
-						paths_processed++
-						if(err && err.length > 0) errors = errors.concat(err)
-						files = files.concat(new_files)
-						done(paths_count, paths_processed, errors, files, callback)
-					})
-				}else{
-					paths_processed++
-					files.push(processing_path)
-					done(paths_count, paths_processed, errors, files, callback)
-				}
-			})
-		})
-	})
+        paths.forEach(function(file){
+
+            var processing_path = path.resolve(dir + path.sep + file)
+
+            fs.lstat( processing_path, function(err, stat){
+                if( err ){
+                    errors.push(err)
+                    paths_processed++
+                    done(paths_count, paths_processed, errors, files, callback)
+                }else if( stat && stat.isDirectory() ){
+                    adr( processing_path, function(err, new_files){
+                        paths_processed++
+                        if(err && err.length > 0)
+                            errors = errors.concat(err)
+                        files = files.concat(new_files)
+                        done(paths_count, paths_processed, errors, files, callback)
+                    })
+                }else{
+                    paths_processed++
+                    files.push(processing_path)
+                    done(paths_count, paths_processed, errors, files, callback)
+                }
+            })
+        })
+    })
 }
 
 function done(total, processed, errors, files, callback){
-	if( total != processed ) return;
-	if( errors && errors.length > 0 ) return callback(errors, files)
-	callback(null, files)
+    if( total != processed ) return;
+    if( errors && errors.length > 0 ) return callback(errors, files)
+    callback(null, files)
 }
